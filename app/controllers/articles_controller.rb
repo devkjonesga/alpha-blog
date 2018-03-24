143fdfbca9d98cbe75bbe12b9e92f63b37411ed9
Kendric Jones <dev.kendric.jones@gmail.com>
2018-03-23 22:04:11 -0400

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit, :update, :show, :destroy]
   def new
     @article = Article.new
   end
@@ -17,28 +18,39 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id]) #moved to private method
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id]) #moved to private method
   end
 
   def update
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id]) #moved to private method
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
     else
       render 'edit'
     end
-
   end
   def index
     #@articles instance variable can be named anything
     @articles = Article.all
   end
+  def destroy
+    #@article = Article.find(params[:id]) #moved to private method
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+
+  end
+
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
